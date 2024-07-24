d5416832caa60a5c547dabd06d8522fad200a094
RamsonAI <callmekali18@gmail.com>
2024-07-24 22:37:17 +0300

    graphql demo

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=300)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
